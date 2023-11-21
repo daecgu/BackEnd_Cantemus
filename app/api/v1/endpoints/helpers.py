98ddba79d4f8adda3187293651359fcaedd6ac2f
Daniel Echeverri <decheverri@helphone.com>
2023-11-21 08:37:44 +0100

    Mejorado formato excepciones, Añadido consultar contador

--- app/api/v1/endpoints/helpers.py
+++ app/api/v1/endpoints/helpers.py
@@ -1,4 +1,5 @@
 from app.models import Contador
+from fastapi import status, HTTPException
 
 
 def crear_o_actualizar_contador(nombre_contador: str) -> int:
@@ -7,4 +8,10 @@ def crear_o_actualizar_contador(nombre_contador: str) -> int:
         contador = Contador(nombre=nombre_contador)
     contador.cuenta += 1
     contador.save()
+    return contador.cuenta
+
+def consultar_contador(nombre_contador:str) -> int:
+    contador = Contador.objects(nombre=nombre_contador).first()  # type: ignore
+    if not contador:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Counter not found")
     return contador.cuenta
--- app/api/v1/endpoints/users.py
+++ app/api/v1/endpoints/users.py
@@ -15,9 +15,9 @@ router = APIRouter(tags=["Gestión Usuarios"])
 
 @router.post("/users/", response_model=UserDisplay, status_code=status.HTTP_201_CREATED)
 async def create_user(user: UserCreate = Body(...)):
-    if UserModel.objects(username=user.username).first(): # type: ignore
+    if UserModel.objects(username=user.username).first():  # type: ignore
         raise HTTPException(
-            status_code=400,
+            status_code=status.HTTP_400_BAD_REQUEST,
             detail=f"El usuario con el nombre {user.username} ya existe."
         )
     hashed_password = crypt.hash(user.password)
@@ -29,7 +29,7 @@ async def create_user(user: UserCreate = Body(...)):
 @router.get("/users/{user_id}", response_model=UserDisplay)
 async def read_user(user_id: str):
     # Busca el usuario por ID
-    user_obj = UserModel.objects(id=user_id).first() # type: ignore
+    user_obj = UserModel.objects(id=user_id).first()  # type: ignore
     if user_obj is None:
         # Si no se encuentra el usuario, lanza una excepción
         raise HTTPException(status_code=404, detail="User not found")
@@ -46,19 +46,19 @@ async def read_user(user_id: str):
 @router.put("/users/{user_id}", response_model=UserDisplay)
 async def update_user(user_id: str, user_update: UserUpdate = Body(...)):
     # Busca el usuario por ID
-    user_obj = UserModel.objects(id=user_id).first() # type: ignore
+    user_obj = UserModel.objects(id=user_id).first()  # type: ignore
     if user_obj is None:
-        raise HTTPException(status_code=404, detail="User not found")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
 
     # Verifica la contraseña actual
     if not crypt.verify(user_update.actualpassword, user_obj.hashed_password):
-        raise HTTPException(status_code=400, detail="Contraseña actual incorrecta")
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Contraseña actual incorrecta")
 
     # Comprobación de usuario y email únicos
-    if user_update.username and UserModel.objects(username=user_update.username, id__ne=user_id).first(): # type: ignore
-        raise HTTPException(status_code=400, detail="El nombre de usuario ya está en uso")
-    if user_update.email and UserModel.objects(email=user_update.email, id__ne=user_id).first(): # type: ignore
-        raise HTTPException(status_code=400, detail="El correo electrónico ya está en uso")
+    if user_update.username and UserModel.objects(username=user_update.username, id__ne=user_id).first():  # type:ignore
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="El nombre de usuario ya está en uso")
+    if user_update.email and UserModel.objects(email=user_update.email, id__ne=user_id).first():  # type: ignore
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="El correo electrónico ya está en uso")
 
     # Actualiza los campos proporcionados
     update_data = user_update.model_dump(exclude_unset=True)
@@ -80,7 +80,7 @@ async def update_user(user_id: str, user_update: UserUpdate = Body(...)):
 @router.delete("/users/{user_id}", response_model=UserDisplay)
 async def delete_user(user_id: str):
     # Busca el usuario por ID
-    user_obj = UserModel.objects(id=user_id).first() # type: ignore
+    user_obj = UserModel.objects(id=user_id).first()  # type: ignore
     if user_obj:
         # Si el usuario existe, elimínalo
         deleted_user = user_obj.to_mongo().to_dict()  # Convertir a diccionario antes de eliminar
@@ -88,6 +88,6 @@ async def delete_user(user_id: str):
         return UserDisplay(**deleted_user)  # Devuelve la representación del usuario eliminado
     else:
         # Si el usuario no se encuentra, lanza una excepción
-        raise HTTPException(status_code=404, detail="User not found")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
 
 # Otros endpoints como autenticación, reseteo de contraseña, etc.
